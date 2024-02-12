944d2ca243246f85a629b339fa2f17278c91de5b
Emil Davlityarov <e.davlityarov@innopolis.university>
2024-02-13 00:31:11 +0300

    New update method

--- bot/src/main/java/edu/java/bot/BotApplication.java
+++ bot/src/main/java/edu/java/bot/BotApplication.java
@@ -1,6 +1,8 @@
 package edu.java.bot;
 
 import edu.java.bot.configuration.ApplicationConfig;
+import edu.java.bot.service.Bot;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -8,7 +10,19 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationConfig.class)
 public class BotApplication {
+    private static ApplicationConfig applicationConfig;
+
+    @Autowired
+    public BotApplication(ApplicationConfig applicationConfig){
+        BotApplication.applicationConfig = applicationConfig;
+    }
     public static void main(String[] args) {
         SpringApplication.run(BotApplication.class, args);
+        Bot telegramBot = new Bot(getToken());
+        telegramBot.processUpdate();
+    }
+
+    private static String getToken(){
+        return applicationConfig.telegramToken();
     }
 }
--- bot/src/main/java/edu/java/bot/service/Bot.java
+++ bot/src/main/java/edu/java/bot/service/Bot.java
@@ -1,2 +1,29 @@
-package edu.java.bot.service;public class Bot {
+package edu.java.bot.service;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import edu.java.bot.configuration.ApplicationConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Bot {
+    private static TelegramBot telegramBot;
+
+
+    public Bot (String telegramToken){
+        telegramBot = new TelegramBot(telegramToken);
+    }
+
+    public void processUpdate(){
+        telegramBot.setUpdatesListener(updates -> {
+            updates.forEach(System.out::println);
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
+        }, e -> {
+            if (e.response() != null){
+                e.response().errorCode();
+                e.response().description();
+            } else {
+                e.printStackTrace();
+            }
+        });
+    }
 }

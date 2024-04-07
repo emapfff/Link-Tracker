595d1d967131c51636bdb6a323de7d4ade991bc2
Emil Davlityarov <e.davlityarov@innopolis.university>
2024-04-07 20:21:33 +0300

    fix

--- scrapper/src/test/java/edu/java/tools/LinkParserTest.java
+++ scrapper/src/test/java/edu/java/tools/LinkParserTest.java
@@ -7,48 +7,49 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.net.URI;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(classes = LinkParser.class)
 class LinkParserTest {
     @Autowired
-    private LinkParser linkParse;
+    private LinkParser linkParser;
 
     @Test
     void parseGithub() {
-        Resource typeLink = linkParse.parse(URI.create("https://github.com/emapfff/java-backend-2024/"));
+        Resource typeLink = linkParser.parse(URI.create("https://github.com/emapfff/java-backend-2024/"));
 
         assertEquals(typeLink, Resource.GITHUB);
     }
 
     @Test
     void parseStackOverFlow() {
-        Resource typeLink = linkParse.parse(URI.create("https://stackoverflow.com/questions/1642028/what-is-the-operator-in-c"));
+        Resource typeLink = linkParser.parse(URI.create("https://stackoverflow.com/questions/1642028/what-is-the-operator-in-c"));
 
         assertEquals(typeLink, Resource.STACKOVERFLOW);
     }
 
     @Test
     void parseIncorrect() {
-        Resource typeLink = linkParse.parse(URI.create("https://mycore"));
+        Resource typeLink = linkParser.parse(URI.create("https://mycore"));
 
         assertEquals(typeLink, Resource.INCORRECT_URL);
     }
 
     @Test
     void getGithubUser() {
-        String user = linkParse.getGithubUser(URI.create("https://github.com/emapfff/java-backend-2024/"));
+        String user = linkParser.getGithubUser(URI.create("https://github.com/emapfff/java-backend-2024/"));
 
         assertEquals(user, "emapfff");
     }
 
     @Test
     void getGithubRepo() {
-        String repo = linkParse.getGithubRepo(URI.create("https://github.com/emapfff/java-backend-2024/"));
+        String repo = linkParser.getGithubRepo(URI.create("https://github.com/emapfff/java-backend-2024/"));
 
         assertEquals(repo, "java-backend-2024");
     }
 
     @Test
     void getStackOverFlowId() {
-        long id = linkParse.getStackOverFlowId(URI.create("https://stackoverflow.com/questions/1642028/what-is-the-operator-in-c"));
+        long id = linkParser.getStackOverFlowId(URI.create("https://stackoverflow.com/questions/1642028/what-is-the-operator-in-c"));
 
         assertEquals(id, 1642028);
     }
